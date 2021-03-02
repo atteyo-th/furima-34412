744c38ecbb191632689675fc555103ad7b242743
atteyo-th <78345265+atteyo-th@users.noreply.github.com>
2021-03-02 15:21:49 +0900

    商品出品機能(itemsテーブル作成)

--- db/migrate/20210302051050_create_items.rb
+++ db/migrate/20210302051050_create_items.rb
@@ -2,9 +2,14 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
 
-      t.string  :content
-      t.references :room, foreign_key: true
-      t.references :user, foreign_key: true
+      t.string     :name,            null: false
+      t.text       :description,     null: false
+      t.integer    :price,           null: false
+      t.integer    :status_id ,      null: false
+      t.integer    :cost_id ,        null: false
+      t.integer    :delivery_id ,    null: false
+      t.integer    :category_id ,    null: false
+      t.references :user,            foreign_key: true
 
       t.timestamps
     end
