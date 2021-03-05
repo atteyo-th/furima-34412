1c05d4aa70f7d4662e4491bc008c8b89d994a981
atteyo-th <78345265+atteyo-th@users.noreply.github.com>
2021-03-05 16:10:15 +0900

    商品情報収集機能(一通り実装)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,15 +22,31 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
-  # def edit
-  #   @item = Item.find(params[:id])
-  #   redirect_to action: :index unless @item.user_id == current_user.id
-  # end
-end
 
-private
+  def edit
+    @item = Item.find(params[:id])
+    # unless@item.user_id == current_user.id
+  end
 
-def item_params
-  params.require(:item).permit(:content, :image, :name, :description, :price, :status_id, :cost_id, :area_id, :delivery_id,
-                               :category_id, :user).merge(user_id: current_user.id)
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  # ストロングパラメーター(updateアクションにて params を指定)
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :price, :status_id, :cost_id, :area_id, :delivery_id, :category_id,
+                                 :user).merge(user_id: current_user.id)
+  end
+
+  # 投稿者以外のユーザーが、編集等で、投稿者専用のページに遷移できないようにする
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
